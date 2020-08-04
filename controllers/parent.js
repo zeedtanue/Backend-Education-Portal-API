f39af14a6265001920a6e7d384f8f90a937a58ad
Tamzeed Hossain <42548887+zeedtanue@users.noreply.github.com>
2020-08-04 18:31:54 +0800

    Update parent.js

--- controllers/parent.js
+++ controllers/parent.js
@@ -2,13 +2,8 @@ const Parent = require('../models/Parents')
 const Student =require('../models/User')
 
 exports.getProfile= async (req,res,next)=>{
-    const parentDB= req.user
-    const userIDs= parentDB.user
-    const userDets=[]
-    for(userID of userIDs){
-        const StudentDB= await Student.findById(userID)
-        userDets.push({name: StudentDB.name,id:StudentDB._id, payment:StudentDB.payment})
-    }
-    res.status(200).json({name: parentDB.name, email:parentDB.email, students:userDets})
+    const parentDB=  await Parent.findById(req.user._id).populate('user')
+    
+    res.status(200).json({parentDB})
 }
 
