129d60fbd3e318228ea89ff07687ade19796cf2b
Tamzeed Hossain <42548887+zeedtanue@users.noreply.github.com>
2020-08-04 18:28:38 +0800

    Update parent.js

--- controllers/parent.js
+++ controllers/parent.js
@@ -7,7 +7,7 @@ exports.getProfile= async (req,res,next)=>{
     const userDets=[]
     for(userID of userIDs){
         const StudentDB= await Student.findById(userID)
-        userDets.push({name: StudentDB.name, payment:StudentDB.payment})
+        userDets.push({name: StudentDB.name,id:StudentDB._id, payment:StudentDB.payment})
     }
     res.status(200).json({name: parentDB.name, email:parentDB.email, students:userDets})
 }
